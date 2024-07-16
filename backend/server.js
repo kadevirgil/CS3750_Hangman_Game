d4ae2c38f4b6292e70d1b69ee122b93a67617309
kade virgil <kadevirgil@mail.weber.edu>
2024-07-16 16:31:30 -0600

    Changed server.js and fixed type in env file

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
 const express = require("express");
-const app = express(); 
+const app = express();
 
-const cors = require("cors")
-const MongoStore = require("connect-mongo"); 
-const session = require("express-session")
-
+const cors = require("cors");
+const session = require("express-session"); 
+const MongoStore = require("connect-mongo");
 
 require("dotenv").config({path: "./config.env"});
 
-const port = process.env.PORT;
+const port = process.env.port; 
 
-app.use(cors(
+app.use(cors( 
     {
-        origin: "http://localhost:3000",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        origin: 'http://localhost:3000',
+        method: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
         optionsSuccessStatus: 204,
-        allowedHeaders: ["Content-Type", "Authorization"]
+        allowedHeaders: ['Content-Type', 'Authorization'],
     }
 ));
 
@@ -30,18 +29,21 @@ app.use(session(
         })
     }
 ));
+const dbo = require("./db/conn");
 
-app.use(express.json());
+app.use(express.json()); 
+
 app.use(require("./routes/records")); 
 
-
-const dbo = require("./db/conn");
+app.get("/", (req,res) => {
+    res.send("Hello World!"); 
+});
 
 app.listen(port, () => {
     dbo.connectToServer(function(err) {
         if (err) {
-            console.err(err); 
+            console.err(err);
         }
     });
-    console.log(`Server is running on port ${port}`); 
+    console.log(`Server is running on port ${port}`);
 });
