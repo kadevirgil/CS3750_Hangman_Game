50d37996a7e72c105bff872efe09c964f0d8cdf6
JulianArchuleta <troyarchuleta@mail.weber.edu>
2024-07-16 17:02:40 -0600

    Added to record.js ADD

--- backend/routes/records.js
+++ backend/routes/records.js
@@ -1,5 +1,35 @@
 const express = require('express');
 
 const routes = express.Router(); 
+// Connect to db
+const dbo = require('../db/conn');
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+//Adding a new user/play session
+routes.route('/records/add').post(async (req, res) =>{
+    try{
+        let db_connect = dbo.getDb();
+        let myobj = {
+            name: req.body.name,
+            numGuesses: 0,
+            lengthOfWord: 0
+        };
+        const account = await db_connect.collection("records").insertOne
+        errorCode = 200;
+        let status = "success";
+        res.status(errorCode).json(status);
+            
+        
+    } catch(err){
+        throw err;
+    }
+    
+    
+
+    res.status(errorCode).json({
+        status: status
+    });
+})
 
 module.exports = routes; 
