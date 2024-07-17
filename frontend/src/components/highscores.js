d42f36ab2c000d9f864b6626c849d37202101e81
kade virgil <kadevirgil@mail.weber.edu>
2024-07-16 18:57:46 -0600

    include useParams

--- frontend/src/components/highscores.js
+++ frontend/src/components/highscores.js
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 const scores = (props) => (
     <tr>
