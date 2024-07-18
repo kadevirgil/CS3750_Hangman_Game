c0456ad6b88f6889fc1dc59ae22f33f2eff2336f
Connor Jones <connorjones1@mail.weber.edu>
2024-07-17 21:42:03 -0600

    Added more UI stuff, didn't fix error on my end

--- frontend/src/components/hangman.js
+++ frontend/src/components/hangman.js
@@ -9,6 +9,21 @@ function GenerateRandomWord() {
 
 }
 
+function Letter({ letter, onLetterClick, disabled }) {
+    return (
+        <button 
+        className="letter"
+        onClick={() => onLetterClick(letter)}
+        disabled={disabled}
+        style={{ backgroundColor: disabled ? 'gray' : '#f0f0f0', 
+            color: 'black', padding: '6px', margin: '5px', 
+            fontSize: '25px', cursor: disabled ? 'not-allowed' : 'pointer' }}
+        >
+            {letter}
+        </button>
+    );
+}
+
 export default function GamePage() {
     const navigate = useNavigate();
     const [user, setUser] = useState({
@@ -17,20 +32,30 @@ export default function GamePage() {
         lengthOfWord: 0
     });
 
+    const [word, setWord] = useState('');
+    const [guessedLetters, setGuessedLetters] = useState([]);
+    const [incorrectGuesses, setIncorrectGuesses] = useState(0);
+
     function updateSession(jsonObj) {
         return setUser((prevJsonObj) => {
             return { ...prevJsonObj, ...jsonObj };
         });
     }
 
-    let word = '';
+    const handleLetterClick = (letter) => {
+        if (!guessedLetters.includes(letter)) {
+          setGuessedLetters([...guessedLetters, letter]);
+          if (!word.includes(letter)) {
+            setIncorrectGuesses(incorrectGuesses + 1);
+          }
+        }
+    };
+
+    let testword = '';
 
     //function to handle the click of a letter
-    function onClickLetter() {
 
-    }
-
-    word = "TestWord"; //Testing purposes
+    
 
     //Not sure if I need this onStartGame function
     async function PlayGame(e) {
@@ -44,19 +69,25 @@ export default function GamePage() {
             window.alert(await response.json())
             return;
         }
-        word = response;
+        //word = response;
         console.log(`The word sent from the backend is ${word}`);
-        setUser({numGuesses: 0, lengthOfWord: word.length});
+        //setUser({numGuesses: 0, lengthOfWord: word.length});
         
     }
 
     //Function to get the number of letter spaces to display to the user
     function PrintWordSpaces() {
-        let wordLength = word.length;
+        //let wordLength = word.length;
+
+        //tests
+        testword = "Sample"; //Testing purposes
+        //let wordL = testword.length;
+        let wordLength = testword.length;//test line just for UX stuff
         console.log(wordLength)
+        //end tests
         let wordSpaces = [];
         for (let i = 0; i < wordLength; i++) {
-            wordSpaces[i] = "____ ";
+            wordSpaces[i] = "_____ ";
         }
         return(<p>{wordSpaces}</p>);
     }
@@ -68,56 +99,46 @@ export default function GamePage() {
                 <div>
                     <label>Start guessing letters!</label>
                 </div>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <div style={{paddingLeft: 50}}>
-                    <PrintWordSpaces/>
+                <div style={{height: '300px'}}>
                 </div>
-                <br/>
-                <br/>
-                <br/>
-                <div>
-                    <div style={{paddingLeft: 50}}>
-                        <button value="A">A</button>
-                        <button value="B">B</button>
-                        <button value="C">C</button>
-                        <button value="D">D</button>
-                        <button value="E">E</button>
-                        <button value="F">F</button>
-                        <button value="G">G</button>
-                        <button value="H">H</button>
-                        <button value="I">I</button>
-                        <button value="J">J</button>
-                        <button value="K">K</button>
-                        <button value="L">L</button>
-                        <button value="M">M</button>
-                        <button value="N">N</button>
+                
+                             
+                <div style={{position: 'relative', bottom: -20}}>
+                    <div style={{paddingLeft: 200, position: 'absolute', bottom: 150}}>
+                        <PrintWordSpaces/>
+                    </div>
+                    <br/>
+                    <br/>
+                    <div style={{paddingLeft: 50, }}>
+                        <Letter letter="A" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("A")}/>
+                        <Letter letter="B" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("B")}/>
+                        <Letter letter="D" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("D")}/>
+                        <Letter letter="C" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("C")}/>
+                        <Letter letter="E" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("E")} />
+                        <Letter letter="F" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("F")} />
+                        <Letter letter="G" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("G")} />
+                        <Letter letter="H" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("H")} />
+                        <Letter letter="I" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("I")} />
+                        <Letter letter="J" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("J")} />
+                        <Letter letter="K" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("K")} />
+                        <Letter letter="L" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("L")} />
+                        <Letter letter="M" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("M")} />
+                        <Letter letter="N" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("N")} />
                         
                     </div>
                     <div style={{paddingLeft: 50}}>
-                        <button value="O">O</button>
-                        <button value="P">P</button>
-                        <button value="Q">Q</button>
-                        <button value="R">R</button>
-                        <button value="S">S</button>
-                        <button value="T">T</button>
-                        <button value="U">U</button>
-                        <button value="V">V</button>
-                        <button value="W">W</button>
-                        <button value="X">X</button>
-                        <button value="Y">Y</button>
-                        <button value="Z">Z</button>                        
+                        <Letter letter="P" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("P")} />
+                        <Letter letter="O" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("O")} />
+                        <Letter letter="Q" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("Q")} />
+                        <Letter letter="R" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("R")} />
+                        <Letter letter="S" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("S")} />
+                        <Letter letter="T" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("T")} />
+                        <Letter letter="U" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("U")} />
+                        <Letter letter="V" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("V")} />
+                        <Letter letter="W" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("W")} />
+                        <Letter letter="X" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("X")} />
+                        <Letter letter="Y" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("Y")} />
+                        <Letter letter="Z" onLetterClick={handleLetterClick} disabled={guessedLetters.includes("Z")} />                       
                     </div>
                 </div>
             
