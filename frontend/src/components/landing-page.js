3b25ec1ded665da79c0e308a6218ca158c88df60
kade virgil <kadevirgil@mail.weber.edu>
2024-07-20 12:10:09 -0600

    Formmated all files, and made minor tweake

--- frontend/src/components/landing-page.js
+++ frontend/src/components/landing-page.js
@@ -1,55 +1,59 @@
-import React from 'react';
-import {useNavigate} from 'react-router';
-import { useState } from 'react';
-
+import React from "react";
+import { useNavigate } from "react-router";
+import { useState } from "react";
 
 export default function LandingPage() {
-    const navigate = useNavigate();
-    const [user, setUser] = useState({
-        name: "",
-        numGuesses: 0,
-        lengthOfWord: 0
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    name: "",
+    numGuesses: 0,
+    lengthOfWord: 0,
+  });
+
+  function updateSession(jsonObj) {
+    return setUser((prevJsonObj) => {
+      return { ...prevJsonObj, ...jsonObj };
     });
-
-    function updateSession(jsonObj) {
-        return setUser((prevJsonObj) => {
-            return { ...prevJsonObj, ...jsonObj };
-        });
+  }
+  async function onStartGame(e) {
+    e.preventDefault();
+    const newPerson = { ...user };
+    const response = await fetch("http://localhost:4000/records/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPerson),
+      credentials: "include",
+    });
+    if (response.status === 400) {
+      //window.alert(await response.json())
+      return;
     }
-    async function onStartGame(e) {
-        e.preventDefault();
-        const newPerson = {...user};
-        const response = await fetch("http://localhost:4000/records/add", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newPerson),
-            credentials: "include",
-            
-        });
-        if (response.status === 400) {
-            //window.alert(await response.json())
-            return;
-        }
-        //window.alert("Sign in for " + newPerson.name);
-        setUser({name: "", numGuesses: 0, lengthOfWord: 0});
-        navigate("/hangman");
-    }
+    //window.alert("Sign in for " + newPerson.name);
+    setUser({ name: "", numGuesses: 0, lengthOfWord: 0 });
+    navigate("/hangman");
+  }
 
-    return(
+  return (
+    <div>
+      <h3>Welcome to Hangman</h3>
+      <form onSubmit={onStartGame}>
         <div>
-            <h3>Welcome to Hangman</h3>
-            <form onSubmit={onStartGame}>
-                <div>
-                    <label>Please enter in your name: </label>
-                    <input type="text" name="name"  value ={user.name} onChange={(e) => updateSession({name: e.target.value})} required/>
-                    <br/>  
-                    <button type="submit" value="Start Game">Start Game</button>
-                </div>
-            </form>
-            
-            
+          <label>Please enter in your name: </label>
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={(e) => updateSession({ name: e.target.value })}
+            required
+          />
+          <br />
+          <button type="submit" value="Start Game">
+            Start Game
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
